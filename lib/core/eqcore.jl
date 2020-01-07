55f340f4adb9cacd255454aa481937e2b24f55b9
ttowncompiled <ian.riley.915@gmail.com>
2020-01-07 11:17:20 -0600

    Places Ranks and Suits in their own module to name protect the enums.

--- lib/core/eqcore.jl
+++ lib/core/eqcore.jl
@@ -2,15 +2,38 @@
 
 module EQCore
 
-    export Value, Suit, Card, Hole
+    export Ranks, Suits, Card, Hole
 
-    @enum Value two=2 three=3 four=4 five=5 six=6 seven=7 eight=8 nine=9 ten=10 jack=11 queen=12 king=13 ace=14
+    module Ranks
+        @enum Rank begin
+            TWO=2
+            THREE=3
+            FOUR=4
+            FIVE=5
+            SIX=6
+            SEVEN=7
+            EIGHT=8
+            NINE=9
+            TEN=10
+            JACK=11
+            QUEEN=12
+            KING=13
+            ACE=14
+        end
+    end
 
-    @enum Suit clubs=1 diamonds=2 hearts=3 spades=4
+    module Suits
+        @enum Suit begin
+            CLUBS=1
+            DIAMONDS=2
+            HEARTS=3
+            SPADES=4
+        end
+    end
 
     struct Card
-        value::Value
-        suit::Suit
+        rank::Ranks.Rank
+        suit::Suits.Suit
     end
 
     struct Hole
