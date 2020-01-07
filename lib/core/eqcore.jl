e306ced4c44d8f4c3b41adc232b23734f87ac53a
ttowncompiled <ian.riley.915@gmail.com>
2020-01-07 11:06:25 -0600

    Exports Value, Suit, Card, and Hole from EQCore.

--- lib/core/eqcore.jl
+++ lib/core/eqcore.jl
@@ -2,6 +2,8 @@
 
 module EQCore
 
+    export Value, Suit, Card, Hole
+
     @enum Value two=2 three=3 four=4 five=5 six=6 seven=7 eight=8 nine=9 ten=10 jack=11 queen=12 king=13 ace=14
 
     @enum Suit clubs=1 diamonds=2 hearts=3 spades=4
