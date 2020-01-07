90a71367df3e388860e5da6265fe3d83816c6a12
ttowncompiled <ian.riley.915@gmail.com>
2020-01-07 14:30:04 -0600

    Removes modules to avoid namespace collisions.

--- lib/core/eq-core.jl
+++ lib/core/eq-core.jl
@@ -1,93 +1,87 @@
-module EQCore
-
-    export Ranks, Suits, Rankings, Card, compare, Hole, Board, Hand
-
-    module Ranks
-        @enum Rank begin
-            TWO=2
-            THREE=3
-            FOUR=4
-            FIVE=5
-            SIX=6
-            SEVEN=7
-            EIGHT=8
-            NINE=9
-            TEN=10
-            JACK=11
-            QUEEN=12
-            KING=13
-            ACE=14
-        end
+module Ranks
+    @enum Rank begin
+        TWO=2
+        THREE=3
+        FOUR=4
+        FIVE=5
+        SIX=6
+        SEVEN=7
+        EIGHT=8
+        NINE=9
+        TEN=10
+        JACK=11
+        QUEEN=12
+        KING=13
+        ACE=14
     end
+end
 
-    module Suits
-        @enum Suit begin
-            CLUBS=1
-            DIAMONDS=2
-            HEARTS=3
-            SPADES=4
-        end
+module Suits
+    @enum Suit begin
+        CLUBS=1
+        DIAMONDS=2
+        HEARTS=3
+        SPADES=4
     end
+end
 
-    module Rankings
-        @enum Ranking begin
-            HIGH=1
-            PAIR=2
-            DUBS=3
-            TRIP=4
-            STRAIGHT=5
-            FLUSH=6
-            FULL=7
-            QUAD=8
-            SFLUSH=9
-            ROYAL=10
-        end
+module Rankings
+    @enum Ranking begin
+        HIGH=1
+        PAIR=2
+        DUBS=3
+        TRIP=4
+        STRAIGHT=5
+        FLUSH=6
+        FULL=7
+        QUAD=8
+        SFLUSH=9
+        ROYAL=10
     end
+end
 
-    struct Card
-        rank::Ranks.Rank
-        suit::Suits.Suit
+struct Card
+    rank::Ranks.Rank
+    suit::Suits.Suit
+end
+
+function compare(c1::Card, c2::Card, tie_break=false)
+    if c1.rank > c2.rank
+        1
+    elseif c1.rank < c2.rank
+        -1
+    elseif ! tie_break
+        0
+    elseif c1.suit > c2.suit
+        1
+    elseif c1.suit < c2.suit
+        -1
+    else
+        0
     end
+end
 
-    function compare(c1::Card, c2::Card, tie_break=false)
-        if c1.rank > c2.rank
-            1
-        elseif c1.rank < c2.rank
-            -1
-        elseif ! tie_break
-            0
-        elseif c1.suit > c2.suit
-            1
-        elseif c1.suit < c2.suit
-            -1
-        else
-            0
-        end
-    end
+struct Hole
+    playerID::Int
+    card1::Card
+    card2::Card
+end
 
-    struct Hole
-        playerID::Int
-        card1::Card
-        card2::Card
-    end
+struct Board
+    flop1::Card
+    flop2::Card
+    flop3::Card
+    turn::Card
+    river::Card
+end
 
-    struct Board
-        flop1::Card
-        flop2::Card
-        flop3::Card
-        turn::Card
-        river::Card
-    end
-
-    struct Hand
-        playerID::Int
-        ranking::Rankings.Ranking
-        card1::Card
-        card2::Card
-        card3::Card
-        card4::Card
-        card5::Card
-    end
-
+struct Hand
+    playerID::Int
+    ranking::Rankings.Ranking
+    card1::Card
+    card2::Card
+    card3::Card
+    card4::Card
+    card5::Card
 end
 
--- lib/core/score-engine.jl
+++ lib/core/score-engine.jl
@@ -1,260 +1,252 @@
-module ScoreEngine
+function score(hole::Hole, board::Board)
+    cards::Array{Card} = _assemble_cards(hole, board)
+    _sort_cards!(cards)
+    best_hand = _royal_flush_draw(cards)
+    if best_hand != nothing
+        return Hand(hole.playerID, Rankings.ROYAL, best_hand...)
+    end
+    best_hand = _straight_flush_draw(cards)
+    if best_hand != nothing
+        return Hand(hole.playerID, Rankings.SFLUSH, best_hand...)
+    end
+    best_hand = _quad_draw(cards)
+    if best_hand != nothing
+        return Hand(hole.playerID, Rankings.QUAD, best_hand...)
+    end
+    best_hand = _full_draw(cards)
+    if best_hand != nothing
+        return Hand(hole.playerID, Rankings.FULL, best_hand...)
+    end
+    best_hand = _flush_draw(cards)
+    if best_hand != nothing
+        return Hand(hole.playerID, Rankings.FLUSH, best_hand...)
+    end
+    best_hand = _straight_draw(cards)
+    if best_hand != nothing
+        return Hand(hole.playerID, Rankings.STRAIGHT, best_hand...)
+    end
+    best_hand = _trip_draw(cards)
+    if best_hand != nothing
+        return Hand(hole.playerID, Rankings.TRIP, best_hand...)
+    end
+    best_hand = _dubs_draw(cards)
+    if best_hand != nothing
+        return Hand(hole.playerID, Rankings.DUBS, best_hand...)
+    end
+    best_hand = _pair_draw(cards)
+    if best_hand != nothing
+        return Hand(hole.playerID, Rankings.PAIR, best_hand...)
+    end
+    best_hand = _high_draw(cards)
+    if best_hand != nothing
+        return Hand(hole.playerID, Rankings.HIGH, best_hand...)
+    end
+    return nothing
+end
 
-    include("eq-core.jl")
+function _assemble_cards(hole::Hole, board::Board)
+    cards = [
+        hole.card1,
+        hole.card2,
+        board.flop1,
+        board.flop2,
+        board.flop3,
+        board.turn,
+        board.river
+    ]
+    return cards
+end
 
-    export score, _assemble_cards, _sort_cards!
+function _sort_cards!(cards::Array{Card})
+    i = 2
+    while i <= 7
+        j = i
+        while j > 1
+            if compare(cards[j], cards[j-1], true) > 0
+                tmp = cards[j]
+                cards[j] = cards[j-1]
+                cards[j-1] = tmp
+                j -= 1
+            else
+                break
+            end
+        end
+        i += 1
+    end
+end
 
-    function score(hole::EQCore.Hole, board::EQCore.Board)
-        cards::Array{EQCore.Card, 7} = _assemble_cards(hole, board)
-        _sort_cards!(cards)
-        best_hand = _royal_flush_draw(cards)
-        if best_hand != nothing
-            return EQCore.Hand(hole.playerID, EQCore.Rankings.ROYAL, best_hand...)
-        end
-        best_hand = _straight_flush_draw(cards)
-        if best_hand != nothing
-            return EQCore.Hand(hole.playerID, EQCore.Rankings.SFLUSH, best_hand...)
-        end
-        best_hand = _quad_draw(cards)
-        if best_hand != nothing
-            return EQCore.Hand(hole.playerID, EQCore.Rankings.QUAD, best_hand...)
-        end
-        best_hand = _full_draw(cards)
-        if best_hand != nothing
-            return EQCore.Hand(hole.playerID, EQCore.Rankings.FULL, best_hand...)
-        end
-        best_hand = _flush_draw(cards)
-        if best_hand != nothing
-            return EQCore.Hand(hole.playerID, EQCore.Rankings.FLUSH, best_hand...)
-        end
-        best_hand = _straight_draw(cards)
-        if best_hand != nothing
-            return EQCore.Hand(hole.playerID, EQCore.Rankings.STRAIGHT, best_hand...)
-        end
-        best_hand = _trip_draw(cards)
-        if best_hand != nothing
-            return EQCore.Hand(hole.playerID, EQCore.Rankings.TRIP, best_hand...)
-        end
-        best_hand = _dubs_draw(cards)
-        if best_hand != nothing
-            return EQCore.Hand(hole.playerID, EQCore.Rankings.DUBS, best_hand...)
-        end
-        best_hand = _pair_draw(cards)
-        if best_hand != nothing
-            return EQCore.Hand(hole.playerID, EQCore.Rankings.PAIR, best_hand...)
-        end
-        best_hand = _high_draw(cards)
-        if best_hand != nothing
-            return EQCore.Hand(hole.playerID, EQCore.Rankings.HIGH, best_hand...)
-        end
+function _royal_flush_draw(cards)
+    if length(cards) < 5
         return nothing
     end
-
-    function _assemble_cards(hole::EQCore.Hole, board::EQCore.Board)
-        cards = [
-            hole.card1,
-            hole.card2,
-            board.flop1,
-            board.flop2,
-            board.flop3,
-            board.turn,
-            board.river
-        ]
-        return cards
+    straight_flush = _straight_flush_draw(cards)
+    if straight_flush == nothing
+        return nothing
     end
+    if straight_flush[1].rank == Ranks.ACE
+        return straight_flush
+    end
+    return nothing
+end
 
-    function _sort_cards!(cards::Array{EQCore.Card, 7})
-        i = 2
-        while i <= 7
-            j = i
-            while j > 1
-                if EQCore.compare(cards[j], cards[j-1], true) > 0
-                    tmp = cards[j]
-                    cards[j] = cards[j-1]
-                    cards[j-1] = tmp
-                    j -= 1
-                else
-                    break
+function _straight_flush_draw(cards)
+    if length(cards) < 5
+        return nothing
+    end
+    straight = _straight_draw(cards)
+    if straight == nothing
+        return nothing
+    end
+    return _flush_draw(straight)
+end
+
+function _quad_draw(cards)
+    if length(cards) < 5
+        return nothing
+    end
+    i = 1
+    while i+3 <= length(cards)
+        if cards[i].rank == cards[i+1].rank && cards[i+1].rank == cards[i+2].rank && cards[i+2].rank == cards[i+3].rank
+            if i == 1
+                return cards[1:5]
+            else
+                return vcat(cards[i:(i+3)], cards[1:1])
+            end
+        end
+        i += 1
+    end
+    return nothing
+end
+
+function _full_draw(cards)
+    if length(cards) < 5
+        return nothing
+    end
+    i = 1
+    while i+2 <= length(cards)
+        if cards[i].rank == cards[i+1].rank && cards[i+1].rank == cards[i+2].rank
+            j = 1
+            while j+1 < i
+                if cards[j].rank == cards[j+1].rank
+                    return vcat(cards[i:(i+2)], cards[j:(j+1)])
                 end
+                j += 1
             end
-            i += 1
-        end
-    end
-
-    function _royal_flush_draw(cards)
-        if length(cards) < 5
-            return nothing
-        end
-        straight_flush = _straight_flush_draw(cards)
-        if straight_flush == nothing
-            return nothing
-        end
-        if straight_flush[1].rank == EQCore.Ranks.ACE
-            return straight_flush
-        end
-        return nothing
-    end
-
-    function _straight_flush_draw(cards)
-        if length(cards) < 5
-            return nothing
-        end
-        straight = _straight_draw(cards)
-        if straight == nothing
-            return nothing
-        end
-        return _flush_draw(straight)
-    end
-
-    function _quad_draw(cards)
-        if length(cards) < 5
-            return nothing
-        end
-        i = 1
-        while i+3 <= length(cards)
-            if cards[i].rank == cards[i+1].rank && cards[i+1].rank == cards[i+2].rank && cards[i+2].rank == cards[i+3].rank
-                if i == 1
-                    return cards[1:5]
-                else
-                    return vcat(cards[i:(i+3)], cards[1:1])
+            j = i+3
+            while j+1 <= length(cards)
+                if cards[j].rank == cards[j+1].rank
+                    return vcat(cards[i:(i+2)], cards[j:(j+1)])
                 end
+                j += 1
             end
-            i += 1
         end
+        i += 1
+    end
+    return nothing
+end
+
+function _flush_draw(cards)
+    if length(cards) < 5
         return nothing
     end
+    i = 1
+    while i+4 <= length(cards)
+        if cards[i].suit == cards[i+1].suit && cards[i+1].suit == cards[i+2].suit && cards[i+2].suit == cards[i+3].suit && cards[i+3].suit == cards[i+4].suit
+            return cards[i:(i+4)]
+        end
+        i += 1
+    end
+    return nothing
+end
 
-    function _full_draw(cards)
-        if length(cards) < 5
-            return nothing
-        end
-        i = 1
-        while i+2 <= length(cards)
-            if cards[i].rank == cards[i+1].rank && cards[i+1].rank == cards[i+2].rank
-                j = 1
-                while j+1 < i
-                    if cards[j].rank == cards[j+1].rank
-                        return vcat(cards[i:(i+2)], cards[j:(j+1)])
-                    end
-                    j += 1
-                end
-                j = i+3
-                while j+1 <= length(cards)
-                    if cards[j].rank == cards[j+1].rank
-                        return vcat(cards[i:(i+2)], cards[j:(j+1)])
-                    end
-                    j += 1
-                end
-            end
-            i += 1
-        end
+function _straight_draw(cards)
+    if length(cards) < 5
         return nothing
     end
+    i = 1
+    while i+4 <= length(cards)
+        if Int(cards[i].rank)-1 == Int(cards[i+1].rank) && Int(cards[i+1].rank)-1 == Int(cards[i+2].rank) && Int(cards[i+2].rank)-1 == Int(cards[i+3].rank) && Int(cards[i+3].rank)-1 == Int(cards[i+4].rank)
+            return cards[i:(i+4)]
+        end
+        i += 1
+    end
+    return nothing
+end
 
-    function _flush_draw(cards)
-        if length(cards) < 5
-            return nothing
-        end
-        i = 1
-        while i+4 <= length(cards)
-            if cards[i].suit == cards[i+1].suit && cards[i+1].suit == cards[i+2].suit && cards[i+2].suit == cards[i+3].suit && cards[i+3].suit == cards[i+4].suit
-                return cards[i:(i+4)]
-            end
-            i += 1
-        end
+function _trip_draw(cards)
+    if length(cards) < 5
         return nothing
     end
-
-    function _straight_draw(cards)
-        if length(cards) < 5
-            return nothing
-        end
-        i = 1
-        while i+4 <= length(cards)
-            if Int(cards[i].rank)-1 == Int(cards[i+1].rank) && Int(cards[i+1].rank)-1 == Int(cards[i+2].rank) && Int(cards[i+2].rank)-1 == Int(cards[i+3].rank) && Int(cards[i+3].rank)-1 == Int(cards[i+4].rank)
-                return cards[i:(i+4)]
+    i = 1
+    while i+2 <= length(cards)
+        if cards[i].rank == cards[i+1].rank && cards[i+1].rank == cards[i+2].rank
+            if i == 1
+                return cards[1:5]
+            elseif i == 2
+                return vcat(cards[2:4], cards[1:1], cards[5:5])
+            else
+                return vcat(cards[i:(i+2)], cards[1:2])
             end
-            i += 1
         end
+        i += 1
+    end
+    return nothing
+end
+
+function _dubs_draw(cards::Array{Card})
+    if length(cards) < 5
         return nothing
     end
-
-    function _trip_draw(cards)
-        if length(cards) < 5
-            return nothing
-        end
-        i = 1
-        while i+2 <= length(cards)
-            if cards[i].rank == cards[i+1].rank && cards[i+1].rank == cards[i+2].rank
-                if i == 1
-                    return cards[1:5]
-                elseif i == 2
-                    return vcat(cards[2:4], cards[1:1], cards[5:5])
-                else
-                    return vcat(cards[i:(i+2)], cards[1:2])
-                end
-            end
-            i += 1
-        end
-        return nothing
-    end
-
-    function _dubs_draw(cards)
-        if length(cards) < 5
-            return nothing
-        end
-        i = 1
-        while i+1 <= length(cards)
-            if cards[i].rank == cards[i+1].rank
-                j = i+2
-                while j+1 <= length(cards)
-                    if cards[j].rank == cards[j+1].rank
-                        if i == 1
-                            if j == 3
-                                return cards[1:5]
-                            else
-                                return vcat(cards[1:2], cards[j:(j+1)], cards[3:3])
-                            end
+    i = 1
+    while i+1 <= length(cards)
+        if cards[i].rank == cards[i+1].rank
+            j = i+2
+            while j+1 <= length(cards)
+                if cards[j].rank == cards[j+1].rank
+                    if i == 1
+                        if j == 3
+                            return cards[1:5]
                         else
-                            return vcat(cards[i:(i+1)], cards[j:(j+1)], cards[1:1])
+                            return vcat(cards[1:2], cards[j:(j+1)], cards[3:3])
                         end
+                    else
+                        return vcat(cards[i:(i+1)], cards[j:(j+1)], cards[1:1])
                     end
-                    j += 1
                 end
+                j += 1
             end
-            i += 1
         end
+        i += 1
+    end
+    return nothing
+end
+
+function _pair_draw(cards::Array{Card})
+    if length(cards) < 5
         return nothing
     end
-
-    function _pair_draw(cards)
-        if length(cards) < 5
-            return nothing
-        end
-        i = 1
-        while i+1 <= length(cards)
-            if cards[i].rank == cards[i+1].rank
-                if i == 1
-                    return cards[1:5]
-                elseif i == 2
-                    return vcat(cards[2:3], cards[1:1], cards[4:5])
-                elseif i == 3
-                    return vcat(cards[3:4], cards[1:2], cards[5:5])
-                else
-                    return vcat(cards[i:(i+1)], cards[1:3])
-                end
+    i = 1
+    while i+1 <= length(cards)
+        if cards[i].rank == cards[i+1].rank
+            if i == 1
+                return cards[1:5]
+            elseif i == 2
+                return vcat(cards[2:3], cards[1:1], cards[4:5])
+            elseif i == 3
+                return vcat(cards[3:4], cards[1:2], cards[5:5])
+            else
+                return vcat(cards[i:(i+1)], cards[1:3])
             end
-            i += 1
         end
+        i += 1
+    end
+    return nothing
+end
+
+function _high_draw(cards::Array{Card})
+    if length(cards) < 5
         return nothing
     end
-
-    function _high_draw(cards)
-        if length(cards) < 5
-            return nothing
-        end
-        return cards[1:5]
-    end
-
+    return cards[1:5]
 end
 
