bb8e03eb216534e76142c187bca5129476f2cce6
ttowncompiled <ian.riley.915@gmail.com>
2020-03-18 14:20:42 -0500

    Adds support for extensions.

--- main.ts
+++ main.ts
@@ -6,6 +6,11 @@ function main() {
     console.log(eval_input_range("AK"));
     console.log(eval_input_range("AA"));
     console.log(eval_input_range("AcKd"));
+    console.log(eval_input_range("AKs,AKo"));
+    console.log(eval_input_range("AQs+"));
+    console.log(eval_input_range("AQo+"));
+    console.log(eval_input_range("QQ+"));
+    console.log(eval_input_range("AcQc+"));
 }
 
 main();
--- src/eval.ts
+++ src/eval.ts
@@ -7,12 +7,7 @@ export function eval_input_range(input: string): Set<string> {
     let node_types = parser.parse();
     let hand_types = new Set<string>();
     for (let node_type of node_types) {
-        let hands = eval_node_type(node_type);
-        for (let hand of hands) {
-            if (! hand_types.has(hand)) {
-                hand_types.add(hand);
-            }
-        }
+        merge_hands(hand_types, eval_node_type(node_type));
     }
     return hand_types;
 }
@@ -31,16 +26,8 @@ function eval_node_type(node_type: NodeType): Set<string> {
 }
 
 function eval_join(join: Join): Set<string> {
-    let left_rank = token_type_literal(join.left.rank);
-    let left_suit = '';
-    if (join.left.suit != null) {
-        left_suit = token_type_literal((<TokenType> join.left.suit));
-    }
-    let right_rank = token_type_literal(join.right.rank);
-    let right_suit = '';
-    if (join.right.suit != null) {
-        right_suit = token_type_literal((<TokenType> join.right.suit));
-    }
+    let [left_rank, left_suit] = eval_card(join.left);
+    let [right_rank, right_suit] = eval_card(join.right);
     if (left_suit == '' && right_suit == 's') {
         return eval_suited(left_rank, right_rank);
     } else if (left_suit == '' && right_suit == 'o') {
@@ -55,6 +42,33 @@ function eval_join(join: Join): Set<string> {
         return eval_suited_and_offsuit(left_rank, right_rank);
     } else {
         return new Set<string>();
+    }
+}
+
+function eval_range_between(range_between: RangeBetween): Set<string> {
+    return new Set<string>();
+}
+
+function eval_extension(extension: Extension): Set<string> {
+    let join = extension.left;
+    let hand_types = eval_join(join);
+    if (join.left.rank == join.right.rank && join.left.suit == null && join.right.suit == null) {
+        let rank = increment_rank(join.right.rank);
+        while (rank != null) {
+            join.left.rank = rank;
+            join.right.rank = rank;
+            hand_types = merge_hands(hand_types, eval_join(join));
+            rank = increment_rank(join.right.rank);
+        }
+        return hand_types;
+    } else {
+        let right_rank = increment_rank(join.right.rank);
+        while (! (right_rank == null || right_rank == join.left.rank)) {
+            join.right.rank = right_rank;
+            hand_types = merge_hands(hand_types, eval_join(join));
+            right_rank = increment_rank(join.right.rank);
+        }
+        return hand_types;
     }
 }
 
@@ -116,10 +130,38 @@ function eval_pocket_pair(rank: string): Set<string> {
     return hand_types;
 }
 
-function eval_range_between(range_between: RangeBetween): Set<string> {
-    return new Set<string>();
+function eval_card(card: Card): [string, string] {
+    let left_rank = token_type_literal(card.rank);
+    let left_suit = '';
+    if (card.suit != null) {
+        left_suit = token_type_literal((<TokenType> card.suit));
+    }
+    return [left_rank, left_suit];
 }
 
-function eval_extension(extension: Extension): Set<string> {
-    return new Set<string>();
+function increment_rank(ttype: TokenType): TokenType | null {
+    switch (ttype) {
+        case TokenType.King: return TokenType.Ace;
+        case TokenType.Queen: return TokenType.King;
+        case TokenType.Jack: return TokenType.Queen;
+        case TokenType.Ten: return TokenType.Jack;
+        case TokenType.Nine: return TokenType.Ten;
+        case TokenType.Eight: return TokenType.Nine;
+        case TokenType.Seven: return TokenType.Eight;
+        case TokenType.Six: return TokenType.Seven;
+        case TokenType.Five: return TokenType.Six;
+        case TokenType.Four: return TokenType.Five;
+        case TokenType.Three: return TokenType.Four;
+        case TokenType.Two: return TokenType.Three;
+        default: return null;
+    }
+}
+
+function merge_hands(A: Set<string>, B: Set<string>): Set<string> {
+    for (let b of B) {
+        if (! A.has(b)) {
+            A.add(b);
+        }
+    }
+    return A;
 }
